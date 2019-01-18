e16b10294bd975ffd71255278d19e97bb5ed3870
Jesse Chung <jesse@soob.co>
2019-01-18 22:56:40 +0900

    Updates

---
+++ precache-manifest.e47d30846310e2aed2f015b2c83bf1c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8abd1c597f853f250d20",
+    "url": "/tezos-certificate/static/js/runtime~main.8abd1c59.js"
+  },
+  {
+    "revision": "4b7e1f3981cb78c0ac54",
+    "url": "/tezos-certificate/static/js/main.4b7e1f39.chunk.js"
+  },
+  {
+    "revision": "a59982609ea80d3c6b82",
+    "url": "/tezos-certificate/static/js/1.a5998260.chunk.js"
+  },
+  {
+    "revision": "4b7e1f3981cb78c0ac54",
+    "url": "/tezos-certificate/static/css/main.677a8ff1.chunk.css"
+  },
+  {
+    "revision": "fa28c226b9f241e764cd8d56b6c3f674",
+    "url": "/tezos-certificate/index.html"
+  }
+];
